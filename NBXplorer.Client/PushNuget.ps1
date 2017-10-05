3025a24f0370f1337c0b643497c6a4b9d46a3b38
NicolasDorier <nicolas.dorier@gmail.com>
2017-10-05 23:39:59 +0900

    fix tag name

--- NBXplorer.Client/PushNuget.ps1
+++ NBXplorer.Client/PushNuget.ps1
@@ -2,5 +2,5 @@ rm "bin\release\" -Recurse -Force
 dotnet pack --configuration Release
 dotnet nuget push "bin\Release\" --source "https://api.nuget.org/v3/index.json"
 $ver = ((ls .\bin\release\*.nupkg)[0].Name -replace 'NBXplorer\.Client\.(\d+(\.\d+){1,3}).*', '$1')
-git tag -a "Client\v$ver" -m "Client\$ver"
+git tag -a "Client/v$ver" -m "Client\$ver"
 git push --tags
