eb195148f6cd0f197c500d4ec5d9eaca13c14b38
bazooka70 <36622310+bazooka70@users.noreply.github.com>
2018-03-29 08:38:21 +0200

    Add files via upload

---
+++ NBXplorer.Client/NBXplorerNetworkProvider.Bitcoincash.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NBXplorer
+{
+    public partial class NBXplorerNetworkProvider
+    {
+		private void InitBitcoincash(ChainType chainType)
+		{
+			NBXplorer.Altcoins.Bitcoincash.Networks.EnsureRegistered();
+			Add(new NBXplorerNetwork()
+			{
+				CryptoCode = "BCH",
+				MinRPCVersion = 140200,
+				NBitcoinNetwork = chainType == ChainType.Main ? NBXplorer.Altcoins.Bitcoincash.Networks.Mainnet:
+								  chainType == ChainType.Test ? NBXplorer.Altcoins.Bitcoincash.Networks.Testnet :
+								  chainType == ChainType.Regtest ? NBXplorer.Altcoins.Bitcoincash.Networks.Regtest : throw new NotSupportedException(chainType.ToString()),
+				DefaultSettings = NBXplorerDefaultSettings.GetDefaultSettings(chainType)
+			});
+		}
+
+		public NBXplorerNetwork GetBCH()
+		{
+			return GetFromCryptoCode("BCH");
+		}
+	}
+}
