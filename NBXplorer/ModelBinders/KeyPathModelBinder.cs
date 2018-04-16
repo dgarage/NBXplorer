1a7dee8db253b2c456ef5b8af5e78318c27bfafd
bazooka70 <36622310+bazooka70@users.noreply.github.com>
2018-04-16 07:32:16 +0200

    issue #38

---
+++ NBXplorer/ModelBinders/KeyPathModelBinder.cs
@@ -0,0 +1,44 @@
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using System.Reflection;
+using NBitcoin;
+using NBXplorer.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NBXplorer.ModelBinders
+{
+	public class KeyPathModelBinder : IModelBinder
+	{
+		#region IModelBinder Members
+
+		public Task BindModelAsync(ModelBindingContext bindingContext)
+		{
+			if (!typeof(KeyPath).GetTypeInfo().IsAssignableFrom(bindingContext.ModelType))
+			{
+				return Task.CompletedTask;
+			}
+
+			ValueProviderResult val = bindingContext.ValueProvider.GetValue(
+				bindingContext.ModelName);
+			if (val == null)
+			{
+				return Task.CompletedTask;
+			}
+
+			string key = val.FirstValue as string;
+			if (key == null)
+			{
+				bindingContext.Model = null;
+				return Task.CompletedTask;
+			}
+
+			var value = new KeyPath(key);
+			bindingContext.Result = ModelBindingResult.Success(value);
+			return Task.CompletedTask;
+		}
+
+		#endregion
+	}
+}
