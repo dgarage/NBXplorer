2c74fc9fcdc025c3799b9fb92f1874b3861a0190
ChekaZ <o.zieglerZ@web.de>
2018-05-22 15:25:16 +0200

    Add Feathercoin (#59)

---
+++ NBXplorer.Client/NBXplorerNetworkProvider.Feathercoin.cs
@@ -0,0 +1,23 @@
+﻿using NBitcoin;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NBXplorer
+{
+    public partial class NBXplorerNetworkProvider
+    {
+		private void InitFeathercoin(NetworkType networkType)
+		{
+			Add(new NBXplorerNetwork(NBitcoin.Altcoins.Feathercoin.Instance, networkType)
+			{
+				MinRPCVersion = 160000
+			});
+		}
+
+		public NBXplorerNetwork GetFTC()
+		{
+			return GetFromCryptoCode(NBitcoin.Altcoins.Feathercoin.Instance.CryptoCode);
+		}
+	}
+}
