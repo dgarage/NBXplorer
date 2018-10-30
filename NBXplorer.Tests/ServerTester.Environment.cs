e81865ffb990049ae70cf0829eab2c735fa5c2ab
David <37987466+dalijolijo@users.noreply.github.com>
2018-10-31 00:21:56 +0100

    Update ServerTester.Environment.cs

--- NBXplorer.Tests/ServerTester.Environment.cs
+++ NBXplorer.Tests/ServerTester.Environment.cs
@@ -55,13 +55,13 @@ namespace NBXplorer.Tests
 			//nodeDownloadData = NodeDownloadData.Groestlcoin.v2_16_0;
 			//Network = NBitcoin.Altcoins.Groestlcoin.Instance.Regtest;
 
-			CryptoCode = "BTX";
-			nodeDownloadData = NodeDownloadData.Bitcore.v0_15_2;
-			Network = NBitcoin.Altcoins.Bitcore.Instance.Regtest;
+			//CryptoCode = "BTX";
+			//nodeDownloadData = NodeDownloadData.Bitcore.v0_15_2;
+			//Network = NBitcoin.Altcoins.Bitcore.Instance.Regtest;
 
-			//CryptoCode = "BTC";
-			//nodeDownloadData = NodeDownloadData.Bitcoin.v0_17_0;
-			//Network = NBitcoin.Network.RegTest;
+			CryptoCode = "BTC";
+			nodeDownloadData = NodeDownloadData.Bitcoin.v0_17_0;
+			Network = NBitcoin.Network.RegTest;
 		}
 	}
 }
