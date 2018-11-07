303529d76282a2c7b3178c8060100d14dc27617e
nicolas.dorier <nicolas.dorier@gmail.com>
2018-11-07 14:04:13 +0900

    change docker tag

--- publish-docker.ps1
+++ publish-docker.ps1
@@ -1,2 +1,2 @@
-docker build -t registry.gitlab.com/metacosa/silo/nbxplorer .
-docker push registry.gitlab.com/metacosa/silo/nbxplorer
+docker build -t registry.gitlab.com/metacosa/silo/nbxplorer:swisscom .
+docker push registry.gitlab.com/metacosa/silo/nbxplorer:swisscom
