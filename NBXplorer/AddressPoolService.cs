77b2bd06bba1492101f70a15a5bc1eef8e8ff2a9
nicolas.dorier <nicolas.dorier@gmail.com>
2019-04-08 14:20:15 +0900

    Prevent NBXplorer stalling with too much altcoins, by having one processing queue for refilling an address pool by altcoin

--- NBXplorer/AddressPoolService.cs
+++ NBXplorer/AddressPoolService.cs
@@ -23,49 +23,63 @@ namespace NBXplorer
 	}
 	public class AddressPoolService : IHostedService
 	{
-		RepositoryProvider _RepositoryProvider;
-		public AddressPoolService(RepositoryProvider repositoryProvider, AddressPoolServiceAccessor accessor)
+		class AddressPool
 		{
-			accessor.Instance = this;
-			_RepositoryProvider = repositoryProvider;
-		}
-		Task _Task;
-		public Task StartAsync(CancellationToken cancellationToken)
-		{
-			_Task = Listen();
-			return Task.CompletedTask;
+			Repository _Repository;
+			Task _Task;
+			internal Channel<(DerivationStrategyBase DerivationStrategy, DerivationFeature Feature)> _Channel = Channel.CreateUnbounded<(DerivationStrategyBase DerivationStrategy, DerivationFeature Feature)>();
+
+			public AddressPool(Repository repository)
+			{
+				_Repository = repository;
+			}
+			public Task StartAsync(CancellationToken cancellationToken)
+			{
+				_Task = Listen();
+				return Task.CompletedTask;
+			}
+			public Task StopAsync(CancellationToken cancellationToken)
+			{
+				_Channel.Writer.Complete();
+				return _Channel.Reader.Completion;
+			}
+			const int RefillBatchSize = 3;
+			private async Task Listen()
+			{
+				while (await _Channel.Reader.WaitToReadAsync() && _Channel.Reader.TryRead(out var item))
+				{
+
+					int generated = 0;
+					while (true)
+					{
+						var count = await _Repository.RefillAddressPoolIfNeeded(item.DerivationStrategy, item.Feature, RefillBatchSize);
+						if (count == 0)
+							break;
+						generated += count;
+					}
+				}
+			}
 		}
 
-		Channel<(NBXplorerNetwork Network, DerivationStrategyBase DerivationStrategy, DerivationFeature Feature)> _Channel = Channel.CreateUnbounded<(NBXplorerNetwork Network, DerivationStrategyBase DerivationStrategy, DerivationFeature Feature)>();
+		public AddressPoolService(NBXplorerNetworkProvider networks, RepositoryProvider repositoryProvider, AddressPoolServiceAccessor accessor)
+		{
+			accessor.Instance = this;
+			_AddressPoolByNetwork = networks.GetAll().ToDictionary(o => o, o => new AddressPool(repositoryProvider.GetRepository(o)));
+		}
+		Dictionary<NBXplorerNetwork, AddressPool> _AddressPoolByNetwork;
+		public Task StartAsync(CancellationToken cancellationToken)
+		{
+			return Task.WhenAll(_AddressPoolByNetwork.Select(kv => kv.Value.StartAsync(cancellationToken)));
+		}
 
 		public void RefillAddressPoolIfNeeded(NBXplorerNetwork network, DerivationStrategyBase derivationStrategy, DerivationFeature feature)
 		{
-			_Channel.Writer.TryWrite((network, derivationStrategy, feature));
-		}
-
-		const int RefillBatchSize = 3;
-		private async Task Listen()
-		{
-			while(await _Channel.Reader.WaitToReadAsync() && _Channel.Reader.TryRead(out var item))
-			{
-				var repo = _RepositoryProvider.GetRepository(item.Network);
-				int generated = 0;
-				while(true)
-				{
-					var count = await repo.RefillAddressPoolIfNeeded(item.DerivationStrategy, item.Feature, RefillBatchSize);
-					if(count == 0)
-						break;
-					generated += count;
-				}
-				//if(generated != 0)
-				//	Logs.Explorer.LogInformation($"{item.Network.CryptoCode}: Generated {generated} addresses for {item.DerivationStrategy.ToPrettyStrategyString()} ({item.Feature})");
-			}
+			_AddressPoolByNetwork[network]._Channel.Writer.TryWrite((derivationStrategy, feature));
 		}
 
 		public Task StopAsync(CancellationToken cancellationToken)
 		{
-			_Channel.Writer.Complete();
-			return _Channel.Reader.Completion;
+			return Task.WhenAll(_AddressPoolByNetwork.Select(kv => kv.Value.StopAsync(cancellationToken)));
 		}
 
 		internal void RefillAddressPoolIfNeeded(NBXplorerNetwork network, TrackedTransaction[] matches)
