d4d458dc476e89144492a3e9e2db755746a9bf4e
Kukks <evilkukka@gmail.com>
2019-11-29 18:34:41 +0100

    Change to extending generate endpoint

--- NBXplorer.Client/Models/GenerateWalletResponse.cs
+++ NBXplorer.Client/Models/GenerateWalletResponse.cs
@@ -1,21 +1,26 @@
-﻿using Newtonsoft.Json;
+﻿using NBitcoin;
+using NBXplorer.DerivationStrategy;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Text;
-using NBitcoin;
+using NBXplorer.JsonConverters;
 
 namespace NBXplorer.Models
 {
-	public class GenerateWalletResponse : ImportWalletResponse
+	public class GenerateWalletResponse
 	{
-		[JsonConverter(typeof(NBXplorer.JsonConverters.MnemonicConverter))]
+		[JsonConverter(typeof(MnemonicConverter))]
 		public Mnemonic Mnemonic { get; set; }
 		public string Passphrase { get; set; }
-
 		[JsonConverter(typeof(NBXplorer.JsonConverters.WordlistJsonConverter))]
 		public NBitcoin.Wordlist WordList { get; set; }
-
 		[JsonConverter(typeof(NBXplorer.JsonConverters.WordcountJsonConverter))]
 		public NBitcoin.WordCount WordCount { get; set; }
+		public BitcoinExtKey MasterHDKey { get; set; }
+		public BitcoinExtKey AccountHDKey { get; set; }
+		[JsonConverter(typeof(NBitcoin.JsonConverters.KeyPathJsonConverter))]
+		public NBitcoin.RootedKeyPath AccountKeyPath { get; set; }
+		public DerivationStrategyBase DerivationScheme { get; set; }
 	}
 }
