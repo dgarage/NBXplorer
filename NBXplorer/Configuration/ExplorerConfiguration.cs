43b606189f7b5063a2c51525abe4ff6ae2598a10
junderw <junderwood@bitcoinbank.co.jp>
2019-12-18 16:21:41 +0900

    GetOrDefault int only

--- NBXplorer/Configuration/ExplorerConfiguration.cs
+++ NBXplorer/Configuration/ExplorerConfiguration.cs
@@ -140,7 +140,7 @@ namespace NBXplorer.Configuration
 					}
 
 					chainConfiguration.StartHeight = config.GetOrDefault<int>($"{network.CryptoCode}.startheight", -1);
-					chainConfiguration.MinUtxoValue = config.GetOrDefault<Money>($"{network.CryptoCode}.minutxovalue", Money.Satoshis(1));
+					chainConfiguration.MinUtxoValue = Money.Satoshis(config.GetOrDefault<int>($"{network.CryptoCode}.minutxovalue", 1));
 					chainConfiguration.HasTxIndex = config.GetOrDefault<bool>($"{network.CryptoCode}.hastxindex", false);
 
 					ChainConfigurations.Add(chainConfiguration);
