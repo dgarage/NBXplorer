66d321c03182f711de421ea9b99fb69831939c8a
nicolas.dorier <nicolas.dorier@gmail.com>
2019-12-19 14:21:05 +0900

    Small refacotring for the MinUTXOValue

--- NBXplorer/ScanUTXOSetService.cs
+++ NBXplorer/ScanUTXOSetService.cs
@@ -172,17 +172,18 @@ namespace NBXplorer
 								try
 								{
 									var result = await scanning.WithCancellation(cts.Token);
-									var OutputsNoSpam = result.Outputs.Where(o => o.Coin.Amount >= repo.MinUtxoValue);
-									var OutputsCount = OutputsNoSpam.Count();
+									var outputs = result.Outputs
+															  .Where(o => o.Coin.Amount >= repo.MinUtxoValue)
+															  .ToArray();
 									var progressObj = workItem.State.Progress.Clone();
 									progressObj.BatchNumber++;
 									progressObj.From += progressObj.Count;
-									progressObj.Found += OutputsCount;
+									progressObj.Found += outputs.Length;
 									progressObj.TotalSearched += scannedItems.Descriptors.Count;
 									progressObj.UpdateRemainingBatches(workItem.Options.GapLimit);
 									progressObj.UpdateOverallProgress();
-									Logs.Explorer.LogInformation($"{workItem.Network.CryptoCode}: Scanning of batch {workItem.State.Progress.BatchNumber} for {workItem.DerivationStrategy.ToPrettyString()} complete with {OutputsCount} UTXOs fetched");
-									await UpdateRepository(rpc, workItem.DerivationStrategy, repo, OutputsNoSpam, scannedItems, progressObj);
+									Logs.Explorer.LogInformation($"{workItem.Network.CryptoCode}: Scanning of batch {workItem.State.Progress.BatchNumber} for {workItem.DerivationStrategy.ToPrettyString()} complete with {outputs.Length} UTXOs fetched");
+									await UpdateRepository(rpc, workItem.DerivationStrategy, repo, outputs, scannedItems, progressObj);
 
 									if (progressObj.RemainingBatches <= -1)
 									{
@@ -236,7 +237,7 @@ namespace NBXplorer
 			}
 		}
 
-		private async Task UpdateRepository(RPCClient client, DerivationSchemeTrackedSource trackedSource, Repository repo, IEnumerable<ScanTxoutOutput> outputs, ScannedItems scannedItems, ScanUTXOProgress progressObj)
+		private async Task UpdateRepository(RPCClient client, DerivationSchemeTrackedSource trackedSource, Repository repo, ScanTxoutOutput[] outputs, ScannedItems scannedItems, ScanUTXOProgress progressObj)
 		{
 			var clientBatch = client.PrepareBatch();
 			var blockIdsByHeight = new ConcurrentDictionary<int, uint256>();
