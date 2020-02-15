6a0c93a076808631b7259b6f8a43f86f36fa4a8f
Justin F. Hallett <thesin@southofheaven.org>
2020-02-15 13:37:10 -0700

    12.2.4 is required

--- NBXplorer.Client/NBXplorerNetworkProvider.Terracoin.cs
+++ NBXplorer.Client/NBXplorerNetworkProvider.Terracoin.cs
@@ -11,7 +11,7 @@ namespace NBXplorer
 		{
 			Add(new NBXplorerNetwork(NBitcoin.Altcoins.Terracoin.Instance, networkType)
 			{
-				MinRPCVersion = 120000
+				MinRPCVersion = 120204
 			});
 		}
 
