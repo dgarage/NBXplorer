a0b6efc1c9cc6082f607d966dd00760f1046c3c8
Oleg Semyonov <osnwt@github.com>
2020-04-17 03:15:00 +0300

    Add Argoneum

---
+++ NBXplorer.Client/NBXplorerNetworkProvider.Argoneum.cs
@@ -0,0 +1,24 @@
+﻿using NBitcoin;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NBXplorer
+{
+    public partial class NBXplorerNetworkProvider
+    {
+		private void InitArgoneum(NetworkType networkType)
+		{
+			Add(new NBXplorerNetwork(NBitcoin.Altcoins.Argoneum.Instance, networkType)
+			{
+				MinRPCVersion = 1040000,
+				CoinType = networkType == NetworkType.Mainnet ? new KeyPath("421'") : new KeyPath("1'")
+			});
+		}
+
+		public NBXplorerNetwork GetAGM()
+		{
+			return GetFromCryptoCode(NBitcoin.Altcoins.Argoneum.Instance.CryptoCode);
+		}
+	}
+}
