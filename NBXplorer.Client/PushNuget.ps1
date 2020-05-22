cc1626594c0f3972e0a28b1b426988f5447846c5
nicolas.dorier <nicolas.dorier@gmail.com>
2020-05-23 07:33:58 +0900

    Publish the pdb with sourcelink for client library

--- NBXplorer.Client/PushNuget.ps1
+++ NBXplorer.Client/PushNuget.ps1
@@ -1,6 +1,7 @@
 rm "bin\release\" -Recurse -Force
-dotnet pack --configuration Release
-dotnet nuget push "bin\Release\" --source "https://api.nuget.org/v3/index.json"
+dotnet pack --configuration Release --include-symbols -p:SymbolPackageFormat=snupkg
+$package=(ls .\bin\Release\*.nupkg).FullName
+dotnet nuget push $package --source "https://api.nuget.org/v3/index.json"
 $ver = ((ls .\bin\release\*.nupkg)[0].Name -replace 'NBXplorer\.Client\.(\d+(\.\d+){1,3}).*', '$1')
 git tag -a "Client/v$ver" -m "Client/$ver"
 git push origin "Client/v$ver"
