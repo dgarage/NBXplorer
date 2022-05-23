37c49b356f2b22d9ff1e9b407ea10a60ad215022
nicolas.dorier <nicolas.dorier@gmail.com>
2022-05-23 13:14:16 +0900

    Fix: descriptors_scripts_unused was causing a full seq scan

---
+++ NBXplorer/DBScripts/009.FasterGetUnused2.sql
@@ -0,0 +1,6 @@
+﻿CREATE OR REPLACE VIEW descriptors_scripts_unused AS
+  SELECT ds.code, ds.descriptor, ds.script, ds.idx, s.addr, d.metadata d_metadata, ds.metadata ds_metadata FROM descriptors_scripts ds
+  JOIN scripts s USING (code, script)
+  JOIN descriptors d
+  USING (code, descriptor)
+  WHERE ds.used IS FALSE;
