50532c4a9236efce2b7f050dd51520eb9a804a94
nicolas.dorier <nicolas.dorier@gmail.com>
2023-11-07 22:12:00 +0900

    Refactor Broadcaster

--- NBXplorer/Broadcaster.cs
+++ NBXplorer/Broadcaster.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.Extensions.Logging;
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Azure.Amqp.Framing;
+using Microsoft.Extensions.Logging;
 using NBitcoin;
 using NBitcoin.RPC;
 using NBXplorer.Backends;
@@ -27,22 +29,31 @@ namespace NBXplorer
 
 		public IIndexers Indexers { get; }
 		public ILoggerFactory LoggerFactory { get; }
-		static string[] missingInputCodes = new string[]
+		record Reject
 		{
-			"Missing inputs",
-			"bad-txns-inputs-spent",
-			"bad-txns-inputs-missingorspent",
-			"txn-mempool-conflict",
-			"missing-inputs",
-			"txn-already-known",
-			"Transaction already in block chain"
+			public record MempoolConflict : Reject;
+			public record MissingInput : Reject;
+			public record InMempoolAlready : Reject;
+			public record NotEnoughFee : Reject;
+			public record Unknown(String RejectReason) : Reject;
+		}
+
+		Reject GetRejectReason(string rejectReason) =>
+		rejectReason switch
+		{
+			"txn-already-in-mempool" => new Reject.InMempoolAlready(),
+			"insufficient fee" => new Reject.MempoolConflict(),
+			"txn-mempool-conflict" => new Reject.MempoolConflict(),
+			{ } s when s.Contains("rejecting replacement", StringComparison.OrdinalIgnoreCase) => new Reject.MempoolConflict(),
+			"Missing inputs" or
+			"bad-txns-inputs-spent" or
+			"bad-txns-inputs-missingorspent" or
+			"txn-already-known" or
+			"missing-inputs" or
+			"Transaction already in block chain" => new Reject.MissingInput(),
+			"mempool min fee not met" => new Reject.NotEnoughFee(),
+			_ => new Reject.Unknown(rejectReason)
 		};
-		private bool IsMissingInput(string rejectReason) =>
-		 missingInputCodes.Contains(rejectReason, StringComparer.OrdinalIgnoreCase) ||
-		IsMempoolConflict(rejectReason);
-		private bool IsMempoolConflict(string rejectReason) =>
-			rejectReason.Equals("txn-mempool-conflict", StringComparison.OrdinalIgnoreCase) ||
-			rejectReason.Contains("rejecting replacement", StringComparison.OrdinalIgnoreCase);
 
 		public async Task<BroadcasterResult> Broadcast(NBXplorerNetwork network, Transaction tx, uint256 transactionId = null)
 		{
@@ -59,24 +70,11 @@ namespace NBXplorer
 				try
 				{
 					var accepted = await rpc.TestMempoolAcceptAsync(tx);
-					if (accepted.IsAllowed)
-						broadcast = true;
-					else if (accepted.RejectReason == "txn-already-in-mempool")
+					if (!accepted.IsAllowed)
 					{
-						result.AlreadyInMempool = true;
-						broadcast = false;
-					}
-					else if (IsMissingInput(accepted.RejectReason))
-					{
-						broadcast = false;
-						result.MissingInput = true;
-						if (IsMempoolConflict(accepted.RejectReason))
-							result.MempoolConflict = true;
-					}
-					else if (accepted.RejectReason == "mempool min fee not met")
-					{
-						broadcast = false;
-						result.NotEnoughFee = true;
+						var rejectReason = GetRejectReason(accepted.RejectReason);
+						SetResult(rejectReason, result);
+						broadcast = rejectReason is Reject.Unknown;
 					}
 				}
 				catch (RPCException ex) when (ex.RPCCode == RPCErrorCode.RPC_METHOD_NOT_FOUND)
@@ -90,24 +88,41 @@ namespace NBXplorer
 					logger.LogInformation($"Rebroadcasted {transactionId}");
 				}
 			}
-			catch (RPCException ex) when (ex.Message == "txn-already-in-mempool")
+			catch (RPCException ex)
+			{
+				var rejectReason = GetRejectReason(ex.Message);
+				if (ex.RPCCode == RPCErrorCode.RPC_TRANSACTION_ALREADY_IN_CHAIN)
+					rejectReason = new Reject.MissingInput();
+				SetResult(rejectReason, result);
+				if (rejectReason is Reject.Unknown u)
+					logger.LogInformation($"Unknown exception when broadcasting {tx.GetHash()} ({u.RejectReason})");
+			}
+			return result;
+		}
+
+		private void SetResult(Reject rejectReason, BroadcasterResult result)
+		{
+			if (rejectReason is Reject.InMempoolAlready)
 			{
 				result.AlreadyInMempool = true;
 			}
-			catch (RPCException ex) when (
-			ex.RPCCode == RPCErrorCode.RPC_TRANSACTION_ALREADY_IN_CHAIN ||
-			IsMissingInput(ex.Message))
+			else if (rejectReason is Reject.MissingInput)
 			{
 				result.MissingInput = true;
-				if (IsMempoolConflict(ex.Message))
-					result.MempoolConflict = true;
 			}
-			catch (Exception ex)
+			else if (rejectReason is Reject.MempoolConflict)
+			{
+				result.MissingInput = true;
+				result.MempoolConflict = true;
+			}
+			else if (rejectReason is Reject.NotEnoughFee)
+			{
+				result.NotEnoughFee = true;
+			}
+			else if (rejectReason is Reject.Unknown)
 			{
 				result.UnknownError = true;
-				logger.LogInformation($"Unknown exception when broadcasting {tx.GetHash()} ({ex.Message})");
 			}
-			return result;
 		}
 	}
 }
