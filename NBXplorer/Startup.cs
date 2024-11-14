fe71dc3703e1616e5053f788e888bb2df152325e
José Manuel Nieto <superjmn@outlook.com>
2024-11-14 09:51:52 +0100

    Add CORS Service

--- NBXplorer/Startup.cs
+++ NBXplorer/Startup.cs
@@ -44,6 +44,7 @@ namespace NBXplorer
 			.AddFormatterMappings();
 			services.AddAuthentication("Basic")
 				.AddNBXplorerAuthentication();
+			services.AddCors();
 		}
 
 		public void Configure(IApplicationBuilder app, IServiceProvider prov,
